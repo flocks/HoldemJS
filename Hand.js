b6877e0a63ca062efedb58faed376169a1157196
Teissier Florent <flocks@MacBook-Air-de-Teissier.local>
2013-06-09 00:49:28 +0200

    hand value

--- Hand.js
+++ Hand.js
@@ -5,11 +5,41 @@ var hand = require('./Card');
 
 function Hand() {
 
+	this.cards = [];
+	this.ranks = [];
+
+	// we set the ranks to 0
+	for(var i = 0; i < 13; i++) {
+		this.ranks[i] = 0;
+	}
+
+}
+
+Hand.prototype.addCard = function(Card) {
+	
+	if (this.cards.length == 5) {
+		return false;
+	}
+
+	this.cards.push(Card);
+
+	this.ranks[Card.getRank()]++;
+
 }
 
 Hand.prototype.compare = function(otherHand) {
-	
+	for (var i = 0; i < 6; i++) {
+		if (this.value[i] > otherHand.value[i]) {
+			return 1;
+		}
+		else if(this.value[i] < otherHand.value[i]) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
 }
 
 
-module.exports = Hands;
+module.exports = Hands;	
