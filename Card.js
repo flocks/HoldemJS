97f9c5958907bf929c00e1ee12411cfea9e40e95
Teissier Florent <flocks@MacBook-Air-de-Teissier.local>
2013-06-08 15:41:16 +0200

    setup classes

--- Card.js
+++ Card.js
@@ -1,5 +1,9 @@
-function Card {
-	
+var ranks = ['Ace','2','3','4','5','6', '7', '8', '9','10','J','Q','K'];
+var suits = ['hearts', 'diamonds', 'spades', 'clubs'];
+
+function Card(rank, suit) {
+	this.rank = rank;
+	this.suit = suit;
 }
 
 
