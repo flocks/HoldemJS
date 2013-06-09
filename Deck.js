5cd13b6a8bf3b02b284df15b6c96a30fef2e3b22
Teissier Florent <flocks@MacBook-Air-de-Teissier.local>
2013-06-09 02:07:06 +0200

    init deck function

--- Deck.js
+++ Deck.js
@@ -2,10 +2,26 @@
 // we suffle up the deck when instanciate
 
 var shuffle = require('./utils/suffle');
-var card = require('./Card.js');
+var Card = require('./Card.js');
 
 function Deck() {
-	
+	this.cards = [];
+	for(var c=0; c < 4; c++) {
+		for(var r=0; r < 13; r++) {
+			var card = new Card(r,c);
+			this.cards.push(card);
+		}
+	}
+}
+
+Deck.prototype.drawCard = function() {
+	var card = this.cards[0];
+	this.cards.slice(0,1);
+	return card;
+}
+
+Deck.prototype.getSizeDeck = function() {
+	return this.cards.length;
 }
 
 module.exports = Deck;
