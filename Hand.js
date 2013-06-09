c5052fddbb41f2482358e6bea661302f2fa95d50
Teissier Florent <flocks@MacBook-Air-de-Teissier.local>
2013-06-09 15:53:34 +0200

    init value computation

--- Hand.js
+++ Hand.js
@@ -3,10 +3,18 @@
 */
 var hand = require('./Card');
 
+
+
 function Hand() {
 
 	this.cards = [];
 	this.ranks = [];
+	this.value = [];
+	this.sameCard = 1;
+	this.sameCards2 = 1;
+	this.smallGroupRank = 0;
+	this.largeGroupRank = 0;
+	this.isFlush = true;
 
 	// we set the ranks to 0
 	for(var i = 0; i < 13; i++) {
@@ -14,6 +22,37 @@ function Hand() {
 	}
 
 }
+
+Hand.prototype.computeValue = function() {
+	// fill the value array of the hand
+
+
+	for (int x=13; x>=1; x--) {
+     if (this.ranks[x] > this.sameCards) {
+         if (this.sameCards != 1)
+         //if sameCards was not the default value
+         {
+             this.sameCards2 = this.sameCards;
+             this.smallGroupRank = this.largeGroupRank;
+         }
+         
+        this.sameCards = this.ranks[x];
+        this.largeGroupRank = x;
+         
+     } else if (this.ranks[x] > this.sameCards2) {
+         this.sameCards2 = this.ranks[x];
+         this.smallGroupRank = x;
+     	}
+	}
+
+	for (int x=0; x<4; x++)  {
+   	 	if ( this.cards[x].getSuit() != this.cards[x+1].getSuit() ) {
+   	 		this.isFlush=false;
+   	 	}
+        
+	}
+}
+
 
 Hand.prototype.addCard = function(Card) {
 	
